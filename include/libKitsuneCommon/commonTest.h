9712e39014ffd52559e9604b16d7b42c4a8e1da0
Tobias Anker <tobias.anker@kitsunemimi.moe>
2018-08-12 19:30:13 +0200

    add common-test-class

---
+++ include/libKitsuneCommon/commonTest.h
@@ -0,0 +1,39 @@
+#ifndef COMMONTEST_H
+#define COMMONTEST_H
+
+#include <string>
+#include <iostream>
+
+namespace Kitsune
+{
+
+class CommonTest
+{
+#define UNITTEST(IS_VAL, SHOULD_VAL) if(IS_VAL != SHOULD_VAL) \
+{  \
+    m_failedTests++; \
+    std::cout << std::endl; \
+    std::cout << "Error in Unit-Test" << std::endl; \
+    std::cout << "   File: " << __FILE__ << std::endl; \
+    std::cout << "   Method: " << __PRETTY_FUNCTION__ << std::endl; \
+    std::cout << "   Line: " << __LINE__ << std::endl; \
+    std::cout << "   Variable: " << #IS_VAL << std::endl; \
+    std::cout << "   Should-Value: " << SHOULD_VAL << std::endl; \
+    std::cout << "   Is-Value: " << IS_VAL << std::endl; \
+    std::cout << std::endl; \
+    } else { \
+    m_successfulTests++; \
+    }
+
+public:
+    CommonTest(std::string testName);
+    ~CommonTest();
+
+protected:
+    uint32_t m_successfulTests = 0;
+    uint32_t m_failedTests = 0;
+};
+
+}
+
+#endif // COMMONTEST_H
--- libKitsuneCommon.pro
+++ libKitsuneCommon.pro
@@ -27,7 +27,9 @@ DEFINES += QT_DEPRECATED_WARNINGS
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-    src/commonThread.cpp
+    src/commonThread.cpp \
+    src/commonTest.cpp
 
 HEADERS += \
-    include/libKitsuneCommon/commonThread.h
+    include/libKitsuneCommon/commonThread.h \
+    include/libKitsuneCommon/commonTest.h
