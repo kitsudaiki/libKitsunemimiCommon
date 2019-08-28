6ed2ecdaf0eb46ccd1c3c2533e20d0a44e6b6f4d
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-08-28 18:09:41 +0200

    add build-script
    Added a bash script to automatically create directories, build the library
    and copy the binary and the includes in a result directory.

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# get current directory-path and the path of the parent-directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PARENT_DIR="$(dirname "$DIR")"
+
+# create build-directory
+BUILD_DIR="$PARENT_DIR/build"
+mkdir -p $BUILD_DIR
+
+# create directory for the final result
+RESULT_DIR="$PARENT_DIR/result"
+mkdir -p $RESULT_DIR
+
+# create build directory for libKitsuneCommon and go into this directory
+LIB_KITSUNE_COMMON_DIR="$BUILD_DIR/libKitsuneCommon"
+mkdir -p $LIB_KITSUNE_COMMON_DIR
+cd $LIB_KITSUNE_COMMON_DIR
+
+# build libKitsuneCommon library with qmake
+/usr/lib/x86_64-linux-gnu/qt5/bin/qmake $DIR/libKitsuneCommon.pro -spec linux-g++
+/usr/bin/make
+
+# copy build-result and include-files into the result-directory
+cp "$LIB_KITSUNE_COMMON_DIR/src/libKitsuneCommon.so.0.4.0" "$RESULT_DIR/"
+cp -r "$DIR/include" "$RESULT_DIR/"
