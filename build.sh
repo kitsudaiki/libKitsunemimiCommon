4215e43ec2b38d1bf760ea970b8b7c607ace4122
Tobias Anker <tobias.anker@kitsunemimi.moe>
2019-10-16 19:42:13 +0200

    rework build-script

--- build.sh
+++ build.sh
@@ -12,21 +12,39 @@ mkdir -p $BUILD_DIR
 RESULT_DIR="$PARENT_DIR/result"
 mkdir -p $RESULT_DIR
 
-# create build directory for libKitsuneCommon and go into this directory
-LIB_KITSUNE_COMMON_DIR="$BUILD_DIR/libKitsuneCommon"
-mkdir -p $LIB_KITSUNE_COMMON_DIR
-cd $LIB_KITSUNE_COMMON_DIR
+#-----------------------------------------------------------------------------------------------------------------
 
-# build libKitsuneCommon library with qmake
-/usr/lib/x86_64-linux-gnu/qt5/bin/qmake $DIR/libKitsuneCommon.pro -spec linux-g++ "CONFIG += optimize_full"
-/usr/bin/make -j4
+function build_kitsune_lib_repo () {
+	REPO_NAME=$1
 
-# copy build-result and include-files into the result-directory
-cp "$LIB_KITSUNE_COMMON_DIR/src/libKitsuneCommon.so.0.6.0" "$RESULT_DIR/"
-cp -r "$DIR/include" "$RESULT_DIR/"
+	# create build directory for repo and go into this directory
+	REPO_DIR="$BUILD_DIR/$REPO_NAME"
+	mkdir -p $REPO_DIR
+	cd $REPO_DIR
 
-# recreate symlinks
-cd "$RESULT_DIR/"
-ln -s libKitsuneCommon.so.0.6.0 libKitsuneCommon.so.0.6
-ln -s libKitsuneCommon.so.0.6.0 libKitsuneCommon.so.0
-ln -s libKitsuneCommon.so.0.6.0 libKitsuneCommon.so
+	# build repo library with qmake
+	/usr/lib/x86_64-linux-gnu/qt5/bin/qmake "$PARENT_DIR/$REPO_NAME/$REPO_NAME.pro" -spec linux-g++ "CONFIG += optimize_full"
+	/usr/bin/make -j4
+
+	# copy build-result and include-files into the result-directory
+	cp -d $REPO_DIR/src/$REPO_NAME.so.* $RESULT_DIR/
+	cp -r $PARENT_DIR/$REPO_NAME/include $RESULT_DIR/
+}
+
+function get_required_kitsune_lib_repo () {
+    REPO_NAME=$1
+    TAG_OR_BRANCH=$2
+
+    # clone repo
+	git clone  git@gitlab.com:tobiasanker/$REPO_NAME.git "$PARENT_DIR/$REPO_NAME"
+	cd "$PARENT_DIR/$REPO_NAME"
+	git checkout $TAG_OR_BRANCH
+
+	build_kitsune_lib_repo $REPO_NAME
+}
+
+#-----------------------------------------------------------------------------------------------------------------
+
+build_kitsune_lib_repo "libKitsuneCommon"
+
+#-----------------------------------------------------------------------------------------------------------------
