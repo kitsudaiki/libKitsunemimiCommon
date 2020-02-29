2a0852cae3d94f6db3b1b9107d67dc089f2b7630
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-02-29 22:32:24 +0100

    add new methods to the ring-buffer

--- include/libKitsunemimiCommon/buffer/ring_buffer.h
+++ include/libKitsunemimiCommon/buffer/ring_buffer.h
@@ -18,20 +18,94 @@ namespace Kitsunemimi
 namespace Network
 {
 
-#define RECV_BUFFER_SIZE 16*1024*1024
+#define RING_BUFFER_SIZE 16*1024*1024
 
 
 struct MessageRingBuffer
 {
-    uint8_t* data = static_cast<uint8_t*>(alignedMalloc(4096, RECV_BUFFER_SIZE));
-    uint64_t totalBufferSize = RECV_BUFFER_SIZE;
+    uint8_t* data = static_cast<uint8_t*>(alignedMalloc(4096, RING_BUFFER_SIZE));
+    uint64_t totalBufferSize = RING_BUFFER_SIZE;
     uint64_t readPosition = 0;
     uint64_t readWriteDiff = 0;
 
     // backup-buffer to collect messages, which are splitted
     // in the data-object
-    uint8_t* overflowBuffer = static_cast<uint8_t*>(alignedMalloc(4096, RECV_BUFFER_SIZE));
+    uint8_t* overflowBuffer = static_cast<uint8_t*>(alignedMalloc(4096, RING_BUFFER_SIZE));
+
+    MessageRingBuffer() {}
+
+    ~MessageRingBuffer()
+    {
+        delete data;
+        delete overflowBuffer;
+    }
 };
+
+/**
+ * @brief getWritePosition
+ * @param recvBuffer
+ * @return
+ */
+uint64_t
+getWritePosition(MessageRingBuffer &recvBuffer)
+{
+    return (recvBuffer.readPosition + recvBuffer.readWriteDiff) % recvBuffer.totalBufferSize;
+}
+
+/**
+ * @brief getSpaceToEnd
+ * @param recvBuffer
+ * @return
+ */
+uint64_t
+getSpaceToEnd(MessageRingBuffer &recvBuffer)
+{
+    const uint64_t writePosition = getWritePosition(recvBuffer);
+
+    uint64_t spaceToEnd = recvBuffer.totalBufferSize - writePosition;
+    if(writePosition < recvBuffer.readPosition) {
+        spaceToEnd = recvBuffer.readPosition - writePosition;
+    }
+
+    return spaceToEnd;
+}
+
+/**
+ * @brief write data into the ring-buffer
+ *
+ * @param recvBuffer ring-buffer-object with the complete data
+ * @param data pointer to the new data
+ * @param dataSize size of the new data
+ *
+ * @return false, if data are bigger than the available space inside the buffer, else true
+ */
+inline bool
+addDataToBuffer(MessageRingBuffer &recvBuffer,
+                const void* data,
+                const uint64_t dataSize)
+{
+    if(dataSize + recvBuffer.readWriteDiff > recvBuffer.totalBufferSize) {
+        return false;
+    }
+
+    const uint64_t writePosition = getWritePosition(recvBuffer);
+    const uint64_t spaceToEnd = getSpaceToEnd(recvBuffer);
+
+    if(dataSize <= spaceToEnd)
+    {
+        memcpy(&recvBuffer.data[writePosition], data, dataSize);
+    }
+    else
+    {
+        const uint64_t remaining = dataSize - spaceToEnd;
+        const uint8_t* dataPos = static_cast<const uint8_t*>(data);
+
+        memcpy(&recvBuffer.data[writePosition], &dataPos[0], spaceToEnd);
+        memcpy(&recvBuffer.data[0], &dataPos[spaceToEnd], remaining);
+    }
+
+    return true;
+}
 
 /**
  * get a pointer to the complete monolitic block of the ring-buffer
