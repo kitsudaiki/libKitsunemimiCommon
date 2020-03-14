ea5246b7c232315c7093c6b55451ef275a653837
Tobias Anker <tobias.anker@kitsunemimi.moe>
2020-03-14 21:43:58 +0100

    update comments in ring-buffer

--- include/libKitsunemimiCommon/buffer/ring_buffer.h
+++ include/libKitsunemimiCommon/buffer/ring_buffer.h
@@ -47,11 +47,11 @@ struct RingBuffer
 
 
 /**
- * @brief getWritePosition
+ * @brief get position to append new data
  *
  * @param ringBuffer reference to ringbuffer-object
  *
- * @return
+ * @return position within the ring-buffer in bytes
  */
 inline uint64_t
 getWritePosition(RingBuffer &ringBuffer)
@@ -60,11 +60,12 @@ getWritePosition(RingBuffer &ringBuffer)
 }
 
 /**
- * @brief getSpaceToEnd
+ * @brief Get number of bytes until the the end of the byte-array or until the the read-positoin.
+ *        It doesn't return the total available space of the ring-buffer.
  *
- * @param ringBuffer
+ * @param ringBuffer reference to ringbuffer-object
  *
- * @return
+ * @return number of bytes until next blocker (end of array or read-position)
  */
 inline uint64_t
 getSpaceToEnd(RingBuffer &ringBuffer)
@@ -166,10 +167,10 @@ getDataPointer(RingBuffer &ringBuffer,
 }
 
 /**
- * @brief moveBufferForward
+ * @brief move the read-position of the ring-buffer forward
  *
  * @param ringBuffer reference to ringbuffer-object
- * @param numberOfBytes
+ * @param numberOfBytes number of bytes to move forward
  */
 inline void
 moveBufferForward(RingBuffer &ringBuffer,
@@ -181,11 +182,12 @@ moveBufferForward(RingBuffer &ringBuffer,
 }
 
 /**
- * @brief getObjectFromBuffer
+ * @brief get a pointer to an object at the beginning of the ring-buffer
  *
  * @param ringBuffer reference to ringbuffer-object
  *
- * @return
+ * @return pointer to requested object within the ring-buffer, but nullptr if there are not enough
+ *         data within the ring-buffer for the requested object
  */
 template <typename T>
 inline const T*
