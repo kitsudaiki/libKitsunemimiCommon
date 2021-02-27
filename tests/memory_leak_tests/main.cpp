5a18d0eb087ac370ec54101061a1dc1f493b7cf2
Tobias Anker <tobias.anker@kitsunemimi.moe>
2021-02-27 13:46:10 +0100

    add memory-leak-tests for item-buffer

--- tests/memory_leak_tests/main.cpp
+++ tests/memory_leak_tests/main.cpp
@@ -9,6 +9,7 @@
 #include <iostream>
 
 #include <libKitsunemimiCommon/buffer/data_buffer_test.h>
+#include <libKitsunemimiCommon/buffer/item_buffer_test.h>
 #include <libKitsunemimiCommon/buffer/ring_buffer_test.h>
 #include <libKitsunemimiCommon/buffer/stack_buffer_reserve_test.h>
 #include <libKitsunemimiCommon/buffer/stack_buffer_test.h>
@@ -24,6 +25,7 @@
 int main()
 {
     Kitsunemimi::DataBuffer_Test();
+    Kitsunemimi::ItemBuffer_Test();
     Kitsunemimi::RingBuffer_Test();
     Kitsunemimi::StackBufferReserve_Test();
     Kitsunemimi::StackBuffer_Test();
